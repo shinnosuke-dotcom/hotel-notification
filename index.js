969de4e9882cadb0cc1959d4b2144b7f6d2a9ea2
shin <qoo080337@gmail.com>
2024-07-05 23:06:08 +0900

    tweet機能

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
-const axios = require('axios');
+const { TwitterApi } = require("twitter-api-v2");
+const axios = require("axios");
+require("dotenv").config();
 
 // 環境変数から基本情報を取得
-const {
-  RAKUTEN_API_KEY,
-  LINE_NOTIFY_TOKEN,
-  HOTEL_ID,
-  RAKUTEN_AFFILIATE_ID
-} = process.env;
+const { RAKUTEN_API_KEY, LINE_NOTIFY_TOKEN, HOTEL_ID, RAKUTEN_AFFILIATE_ID } =
+  process.env;
+
+const twitterClient = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET_KEY,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
 
 // 実行日の次の日を取得
 const getNextDay = () => {
   const today = new Date();
   const nextDay = new Date(today);
   nextDay.setDate(today.getDate() + 1);
-  return nextDay.toISOString().split('T')[0];
+  return nextDay.toISOString().split("T")[0];
 };
 
 // 開始日から3ヶ月後の日付を取得
@@ -22,7 +26,7 @@ const getThreeMonthsLater = (startDate) => {
   const start = new Date(startDate);
   const threeMonthsLater = new Date(start);
   threeMonthsLater.setMonth(start.getMonth() + 3);
-  return threeMonthsLater.toISOString().split('T')[0];
+  return threeMonthsLater.toISOString().split("T")[0];
 };
 
 const START_DATE = getNextDay();
@@ -38,8 +42,8 @@ const getDatesInRange = (startDate, endDate) => {
     const checkout = new Date(start);
     checkout.setDate(checkin.getDate() + 1);
     dates.push({
-      checkinDate: checkin.toISOString().split('T')[0],
-      checkoutDate: checkout.toISOString().split('T')[0]
+      checkinDate: checkin.toISOString().split("T")[0],
+      checkoutDate: checkout.toISOString().split("T")[0],
     });
     start.setDate(start.getDate() + 1);
   }
@@ -53,29 +57,41 @@ const checkAvailability = async () => {
   const { checkinDate, checkoutDate } = dates.shift();
 
   try {
-    const response = await axios.get('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426', {
-      params: {
-        applicationId: RAKUTEN_API_KEY,
-        hotelNo: HOTEL_ID,
-        checkinDate,
-        checkoutDate,
+    const response = await axios.get(
+      "https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426",
+      {
+        params: {
+          applicationId: RAKUTEN_API_KEY,
+          hotelNo: HOTEL_ID,
+          checkinDate,
+          checkoutDate,
+        },
       }
-    });
+    );
 
     const data = response.data;
 
     if (data.hotels && data.hotels.length > 0) {
       const hotelName = data.hotels[0].hotel[0].hotelBasicInfo.hotelName;
-      const hotelURL = data.hotels[0].hotel[1].roomInfo[0].roomBasicInfo.reserveUrl;
+      const hotelURL =
+        data.hotels[0].hotel[1].roomInfo[0].roomBasicInfo.reserveUrl;
 
-      await sendLineNotification(`空室があります！
+      const message = `【空室情報】
 ホテル名: ${hotelName} 
-チェックイン: ${checkinDate} 
-予約ページ: ${hotelURL}`);
+チェックイン日: ${checkinDate} 
+予約ページ: ${hotelURL}
+
+#disney #トイストーリーホテル`;
+
+      await sendTwitterNotification(message);
+      // await sendLineNotification(message);
     }
   } catch (error) {
-    if(error?.response?.status!==404){
-      console.error(`エラーが発生しました (チェックイン: ${checkinDate}):`, error);
+    if (error?.response?.status !== 404) {
+      console.error(
+        `エラーが発生しました (チェックイン: ${checkinDate}):`,
+        error
+      );
     }
   }
 
@@ -84,19 +100,28 @@ const checkAvailability = async () => {
   }
 };
 
+const sendTwitterNotification = async (message) => {
+  try {
+    await twitterClient.v2.tweet(message);
+  } catch (error) {
+    console.error("tweet中にエラーが発生しました:", error);
+  }
+};
+
 const sendLineNotification = async (message) => {
   try {
-    await axios.post('https://notify-api.line.me/api/notify',
+    await axios.post(
+      "https://notify-api.line.me/api/notify",
       new URLSearchParams({ message }),
       {
         headers: {
           Authorization: `Bearer ${LINE_NOTIFY_TOKEN}`,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
     );
   } catch (error) {
-    console.error('LINE通知の送信中にエラーが発生しました:', error);
+    console.error("LINE通知の送信中にエラーが発生しました:", error);
   }
 };
 
